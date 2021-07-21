c7a9d8211e4e25acf923dd9464f4f2cb81e152be
Kevin Choi <kevchoi1028@gmail.com>
2021-07-21 13:30:36 -0700

    test

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send(
     `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
