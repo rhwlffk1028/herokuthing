69b4a29e5944aaf1a7a9da9b5d400ae995d19bbe
Kevin Choi <kevchoi1028@gmail.com>
2021-07-21 13:38:30 -0700

    test2

--- index.js
+++ index.js
@@ -10,15 +10,3 @@ app.get('/', (req, res) => {
     `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
   );
 });
-
-app.get('/api', (req, res) => {
-  res.json({
-    term: 'api',
-    description:
-      'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
-  });
-});
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
